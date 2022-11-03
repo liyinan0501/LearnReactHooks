5b559b1d506f4c6941cd18c385153a3eb113c09b
Yinan Li <gabriel.li5566@gmail.com>
2022-11-03 20:17:32 +0200

    update the comments of useContext

--- src/18-useContext().js
+++ src/18-useContext().js
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import React, { useState, useContext } from 'react'
-const Context = React.createContext() // 如果App和Son不在同一文件中，App需要导出Context，供Son使用
-// useContext()
-// 作用：实现跨组件传递数据，而不必在每个级别手动传递 props，简化组件之间的数据传递过程
+const Context = React.createContext() // 如果App和Son不在同一文件中，App需要导出Context，供Son使用 useContext(Context)。
+//* useContext()
+// 作用：实现跨组件传递数据，而不必在每个级别手动传递 props，简化组件之间的数据传递过程。
 function App() {
   const [color, setColor] = useState('red')
   const [name, setName] = useState('Jack')
@@ -33,6 +33,7 @@ const Son = () => {
   console.log(color)
   return (
     <div>
+      <h5>Son Component - {color[0]}</h5>
       <h5>Son Component - {color[1]}</h5>
     </div>
   )
