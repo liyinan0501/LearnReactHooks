a9a86eeaae01ca1f47072f9bc96ca67f6b877d58
Yinan Li <gabriel.li5566@gmail.com>
2022-11-06 14:20:19 +0200

    update if didint use useCallback()  what will be happened

--- src/20-memo()-useCallback().js
+++ src/20-memo()-useCallback().js
@@ -10,12 +10,16 @@ import React, { useState, memo, useCallback } from 'react'
 // React state 更新原则，不修改原数据（因为地址不会改变），而是覆盖原数据（地址发生改变），从而memo起作用。
 
 //* React.memo()-useCallback()
-// useCallback 可以记住函数的引用，在组件每次更新时都指向相同引用的函数。
+// useCallback 可以记住函数的引用，在本组件每次更新时都指向相同引用的函数。
 //* React.memo()-useMemo()
 // useMemo 可以记住任何数据类型的引用，在组件每次更新时都指向相同引用的数据。
 function App() {
   const [count, setCount] = useState(5)
   const [money, setMoney] = useState(1000)
+  // const extend = () => {
+  //   setCount(count + 1)
+  // }
+
   // 记忆的函数 = useCallback(函数,[依赖])
   // 只要依赖项不变，这个函数就不会变化，如果依赖项变了，这个函数就会变化。
   const extend = useCallback(() => {
@@ -37,7 +41,7 @@ function App() {
     </div>
   )
 }
-// 即使使用了memo，父组件点击Add Money，也会导致Child更新，因为更新Money父组件重新执行，又会有一个新的相同函数赋值给extend，
+// 即使使用了memo，不用useCallback，父组件点击Add Money，也会导致Child更新，因为更新Money父组件重新执行，又会有一个新的相同函数赋值给extend，
 // 因为函数是复杂数据类型，重新赋值也就是extend 地址发生改变，导致Child也会更新，也就是memo的性能优化失效了。
 const Child = memo(({ count, extend }) => {
   console.log('Child updated')
